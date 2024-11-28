5b8bcc1fb75a15a0f670d68cc07664a45e26f314
Luca Deeken <luca.deeken@fuchs-edv.de>
2024-11-28 17:01:33 +0100

    add orderpage amd fix bugs with the ones already existing git status

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import "./styles.css";
 import {getMenu, getDrinks} from "./menu.js";
 import {getStart} from "./start.js";
+import {getOrder} from "./order.js";
 
 const menuButton = document.getElementById("menu");
 const startButton = document.getElementById("start");
-
+const orderButton= document.getElementsByClassName("orderButton");
 
 //Speichert den DOM-Content von "content"
 const content = document.querySelector('#content').innerHTML;
+
+function orderButtonAddEventListener() {
+    for (let i= 0; i<orderButton.length; i++) {
+        orderButton[i].addEventListener("click", () => {
+            getOrder();
+        })
+    };    
+}
+orderButtonAddEventListener();
 
 menuButton.addEventListener("click", () => {
     getMenu();
@@ -18,11 +28,8 @@ menuButton.addEventListener("click", () => {
 
 })
 
-
-
-
 startButton.addEventListener("click", () => {
     getStart();
     document.querySelector('#content').innerHTML = content;
+    orderButtonAddEventListener();
 })
-
